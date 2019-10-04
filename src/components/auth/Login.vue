c9338dd80c7fdf0b508da37e485b87b102b0e9ce
Taylor Quinn <tjquinn1@asu.edu>
2019-10-04 14:11:56 -0700

    fixed login form validation

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -2,14 +2,14 @@
 	v-row(align="center" justify="center")
 		v-col(cols="6")
 			v-card
-				v-form(lazy-validation ref="createProject" v-model="valid")
+				v-form(ref="loginForm" v-model="valid")
 					v-card-title#title Login
 					v-card-text
-						v-text-field(class="mt-4" label="Username" required outlined v-model="username" :rules="loginRules")
-						v-text-field(label="Password" required outlined password :type="show1 ? 'text' : 'password'" v-model="password" :rules="loginRules")
+						v-text-field(class="mt-4" label="Username" required outlined v-model="username" :rules="[() => !!username || 'Username Required.']")
+						v-text-field(label="Password" required outlined password :type="show1 ? 'text' : 'password'" v-model="password" :rules="[() => !!password || 'Password Required.']")
 					v-card-actions()
 						div(class="flex-grow-1")    
-						v-btn(class="mr-4" color="teal" :disabled="valid" large depressed @click="login") Login
+						v-btn(class="mr-4" color="teal" :disabled="!valid" large depressed @click="login") Login
 </template>
 
 <script lang="ts">
@@ -28,12 +28,9 @@ export default class Login extends Vue {
   private error: boolean = false;
 
   private valid: boolean = false;
-  private loginRules = [
-    (value) => value !== '' || 'Required.',
-  ];
 
   public async login(): Promise<void> {
-    if ((this.$refs.createProject as VForm).validate()) {
+    if ((this.$refs.loginForm as VForm).validate()) {
       try {
 
       } catch (e) {
