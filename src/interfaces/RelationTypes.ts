5455f7510f6cb0e2d94c57fca0230b0b9f8c75a1
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2020-01-07 13:20:27 -0700

    [VGNWB-284] Add relation template types

---
+++ src/interfaces/RelationTypes.ts
@@ -0,0 +1,19 @@
+export interface RelationTemplateField {
+	part_field?: string;
+	concept_label?: string;
+	part_id: number;
+	evidence_required?: boolean;
+	description?: string;
+	type?: string;
+	concept_id?: number;
+	label?: string;
+}
+
+export interface RelationTemplate {
+	id: number;
+	created: string;
+	name?: string;
+	description?: string;
+	expression?: string;
+	fields?: [RelationTemplateField];
+}
