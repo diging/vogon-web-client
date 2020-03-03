4cfcbe1416f63f99eb32fe5b228e809ab2c05e4c
Zachary Rudebeck <zachary.rudebeck@gmail.com>
2020-03-03 15:29:20 -0700

    [VGNWB-320] PR requested changes

--- src/views/texts/TextDetails.vue
+++ src/views/texts/TextDetails.vue
@@ -11,10 +11,23 @@
 							h3(class="headline") Resource "{{ text.title }}"
 							v-list-item-subtitle(class="text--primary" v-text="text.uri")
 						v-col(cols="6")
-							div(v-if="project" class="float-right")
-								v-btn(color="primary" @click="removeText")
-									v-icon(left) mdi-minus
+							div(v-if="project && !submitted" class="float-right")
+								v-tooltip(left)
+									template(v-slot:activator="{ on }")
+										v-btn(color="#db1a04" v-on="on" @click="removeText")
+											div(class="delete-button-text")
+												v-icon(left) mdi-trash-can-outline
+									span
 									| Remove from project
+							div(v-else-if="project && submitted" class="float-right")
+								v-tooltip(left)
+									template(v-slot:activator="{ on }")
+										div(v-on="on")
+											v-btn(color="#db1a04" disabled)
+												div(class="delete-button-text")
+													v-icon(left) mdi-trash-can-outline
+									span
+									| Texts cannot be removed after annotations have been submitted
 							div(v-else-if="$route.query.project_id" class="float-right")
 								v-btn(color="primary" @click="addText")
 									v-icon(left) mdi-plus
@@ -77,6 +90,7 @@ export default class TextDetails extends Vue {
 	private text: TextResource = {id: 1, title: ''};
 	private relations: RelationSet[] = [];
 	private masterId: number;
+	private submitted: boolean = true;
 
 	private snackbarText: string = '';
 	private snackbar: boolean = false;
@@ -98,6 +112,7 @@ export default class TextDetails extends Vue {
 				this.project = response.data.part_of_project && response.data.part_of_project.name;
 				this.relations = response.data.relations;
 				this.masterId = response.data.master_text.id;
+				response.data.submitted ? this.submitted = true : this.submitted = false;
 			})
 			.catch(() => this.error = true)
 			.finally(() => this.loading = false);
@@ -120,8 +135,8 @@ export default class TextDetails extends Vue {
 		Vue.$axios.delete(`/project/${this.$route.query.project_id}/delete_text`, {
 				data: { text_id: this.masterId },
 			})
-			.then((response: AxiosResponse) => {
-				this.getTextDetails();
+			.then(() => {
+				this.project = '';
 			})
 			.catch((error) => {
 				this.snackbar = true;
@@ -146,5 +161,9 @@ export default class TextDetails extends Vue {
 }
 .annotation-title {
 	padding: 0 16px 8px;
+}
+.delete-button-text{
+	color: white;
+	padding-left: 10px;
 }
 </style>
