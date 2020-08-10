956c75f2860ca8a9a44cb94fc9ab972815ee985f
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2020-08-10 00:57:31 -0700

    [VGNWB-340] Change cancel button position

--- src/components/annotator/AppellationListItem.vue
+++ src/components/annotator/AppellationListItem.vue
@@ -1,21 +1,23 @@
 <template lang="pug">
 	div(:class="`text-left pa-2 ${focused}`" ref="listItem")
 		v-row
-			v-col(:cols="10" @click="focusAppellation" class="focus-icon")
+			v-col(:cols="7" @click="focusAppellation" class="focus-icon")
 				div(class="subtitle-1") {{ appellation.interpretation.label }}
 				div(class="subtitle-2 appellation-subtitle") Created by <strong>{{ creator }}</strong> on {{ date }}
-				div(v-if="edit") 
-					| (You are currently editing this appellation ...)
-					br
-					v-alert(dense type="error" class="my-4" v-if="appellation.relationsFrom.length || appellation.relationsTo.length")
-						| This appellation is part of existing relation(s) !!
-					v-btn(small dense @click="editAppellation") Cancel Edit
-			v-col(:cols="2" class="text-right")
+				
+			v-col(:cols="5" class="text-right")
+				v-btn(v-if="edit" color="error" class="mr-2" small dense @click="editAppellation") Cancel
 				v-btn(v-if="!appellation.submitted" @click="editAppellation" small icon class="d-inline-block mr-1")
 					v-icon(left :color="edit ? `green` : `default`") mdi-pencil
 				v-btn(@click="toggleVisibility" small icon class="d-inline-block mr-1" :disabled="$store.getters.getAnnotatorHideAppellation")
 					v-icon(v-if="visible" left) mdi-eye
 					v-icon(v-else left) mdi-eye-off
+
+		div(v-if="edit") 
+				| (You are currently editing this appellation ...)
+				br
+				v-alert(dense type="error" class="my-4" v-if="appellation.relationsFrom.length || appellation.relationsTo.length")
+					| This appellation is part of existing relation(s) !!
 </template>
 
 <script lang="ts">
