ad7ad067ccff845eb0b9b925485d13be21c9d04a
tjquinn1 <tjquinn1@asu.edu>
2020-09-10 10:47:38 -0700

    added shared vol

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:13.8
 
 WORKDIR /usr/src/app
 RUN git clone -b docker https://github.com/diging/vogon-web-client.git
-WORKDIR /usr/src/app/vogon-web-client
+WORKDIR /usr/src/app
 RUN ["chmod", "+x", "/usr/src/app/vogon-web-client/startup.sh"]
 
 ENTRYPOINT ["./startup.sh"]
--- startup.sh
+++ startup.sh
@@ -5,9 +5,9 @@ mkdir bin
 apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get autoclean
 # most of these are for lxml which needs a bunch of dependancies installed
 
-mkdir /dist
+mkdir /vogon-client/dist
 npm install
 npm run build
-cp -r /usr/src/app/vogon-web-client/dist /dist/
-source .env
+cp -r /usr/src/app/vogon-web-client/dist /vogon-client/dist/
+source vogon-client/.env
 tail -f /dev/null
