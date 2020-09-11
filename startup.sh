01d6faea026fadf7f773c9479fc19f5e8ab9dc88
tjquinn1 <tjquinn1@asu.edu>
2020-09-11 10:27:50 -0700

    added docker cp

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ mkdir logs/
 mkdir bin
 apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get autoclean
 # most of these are for lxml which needs a bunch of dependancies installed
-docker cp -r vogon-client-vol:.env vogon-client:/vogon-client
+cp -r .env ../vogon-client
 cd ..
 cd vogon-client-vol
 mkdir dist/
@@ -12,4 +12,4 @@ cd ..
 cd vogon-web-client
 npm install
 npm run build
-docker cp -r vogon-client:/usr/src/app/vogon-web-client/dist vogon-client-vol:/usr/src/app/vogon-client/dist/
+cp -r vogon-client:/usr/src/app/vogon-web-client/dist vogon-client-vol:/usr/src/app/vogon-client/dist/
