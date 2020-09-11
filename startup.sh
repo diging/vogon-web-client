1f0b6cf09a5e06732b0c16317693ae4f3c1ee5d7
tjquinn1 <tjquinn1@asu.edu>
2020-09-11 07:48:10 -0700

    updated shell script

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ mkdir bin
 apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get autoclean
 # most of these are for lxml which needs a bunch of dependancies installed
 
-mkdir /vogon-client/dist
+mkdir vogon-client/dist/
 vogon-web-client/npm install
 vogon-web-client/npm run build
 cp -r /usr/src/app/vogon-web-client/dist /usr/src/app/vogon-client/dist/
