7b13c543d3ef42484be87bb034013b42834d4925
tjquinn1 <tjquinn1@asu.edu>
2020-09-11 12:36:41 -0700

    added docker cp

--- startup.sh
+++ startup.sh
@@ -4,8 +4,9 @@ mkdir logs/
 mkdir bin
 apt-get update && apt-get upgrade -y && apt-get autoremove && apt-get autoclean
 # most of these are for lxml which needs a bunch of dependancies installed
-cp  ../.env .
+cp  ../host/.env .
+
 
 npm install
 npm run build
-
+cp -r dist ../host/
