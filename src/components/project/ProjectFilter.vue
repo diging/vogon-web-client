0812258f4eae96ad736c7833dbcfd7250f5bf86a
sudheerad9 <sudheerad9@gmail.com>
2022-01-21 14:02:50 -0700

    [VGNWB-371] Add drop down component

--- src/components/project/ProjectFilter.vue
+++ src/components/project/ProjectFilter.vue
@@ -18,6 +18,7 @@
 					dense
 				)
 			v-col(md="3")
+			<template v-if="!isAdminUser()">
 				v-switch(
 					v-model="showUserProjects"
 					label="Show only my projects"
@@ -27,11 +28,18 @@
 					dense
 					flat
 				)
+			v-else 
+				v-btn(text v-on="on" v-if="loggedIn") Project Filter
+					v-icon mdi-menu-down
+
+			</template>
 		v-btn(depressed color="primary" @click="applyFilter") Apply
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import { getUserId } from '@/utils';
+import { AxiosResponse } from 'axios';
 
 @Component({ name: 'ProjectFilter' })
 export default class ProjectFilter extends Vue {
@@ -40,9 +48,15 @@ export default class ProjectFilter extends Vue {
 	private query: string = '';
 	private field: string = 'name';
 	private showUserProjects: boolean = true;
+	private isAdmin: boolean = false;
 	private fieldChoices: any[] = [
 		{ text: 'Title', value: 'name' },
 		{ text: 'Owner', value: 'owner' },
+	];
+	private dataItems: object[] = [
+		{ title: 'my projects', value: 'my projects' },
+		{ title: 'shared projects', link: 'shared projects' },
+		{ title: 'all projects', link: 'all projects' },
 	];
 
 	@Watch('field')
@@ -54,6 +68,15 @@ export default class ProjectFilter extends Vue {
 
 	private applyFilter() {
 		this.onApply(this.query, this.field, this.showUserProjects);
+	}
+
+	private isAdminUser() {
+		const userId = getUserId();
+		Vue.$axios.get(`/users/${userId}`)
+			.then((response: AxiosResponse) => {
+				this.isAdmin = response.data.is_admin;
+			});
+		return this.isAdmin;
 	}
 }
 </script>
---
+++ src/views/project/test.vue
@@ -0,0 +1,149 @@
+<template lang="pug">
+	div(class="main")
+		v-row
+			v-col(md="6")
+				h2(class="display-1") Projects
+			v-col(md="6")
+				div(class="float-right")
+					CreateUpdateProject(v-bind:getProjectDetails="getProjectDetails")
+
+		v-card(class="card-projects mt-4")
+			ProjectFilter(
+				:onApply="onApplyFilter"
+				class="mb-6"
+			)
+			ErrorIndicator(v-if="error") Error while loading projects!
+			div(v-else)
+				Loading(v-if="loading")
+				template(v-else)
+					template(v-if="!projects.length")
+						EmptyView No projects found!
+					template(v-else)
+						v-list(v-for="(project, index) in projects" :key="project.id" class="project-list")
+							ProjectItem(v-bind:project="project")
+							v-divider(v-if="index + 1 < projects.length")
+
+						v-pagination(
+							v-model="page"
+							:length="Math.ceil(projectsCount / PAGE_SIZE)"
+							v-on:input="getProjectList"
+						)
+
+</template>
+
+<script lang="ts">
+import { AxiosResponse } from 'axios';
+import { Component, Vue } from 'vue-property-decorator';
+
+import EmptyView from '@/components/global/EmptyView.vue';
+import ErrorIndicator from '@/components/global/ErrorIndicator.vue';
+import Loading from '@/components/global/Loading.vue';
+import CreateUpdateProject from '@/components/project/CreateUpdateProject.vue';
+import ProjectFilter from '@/components/project/ProjectFilter.vue';
+import ProjectItem from '@/components/project/ProjectItem.vue';
+import { PAGE_SIZE } from '@/constants';
+import { Project } from '@/interfaces/ProjectTypes';
+
+
+@Component({
+	name: 'ProjectList',
+	components: {
+		Loading,
+		ErrorIndicator,
+		EmptyView,
+		CreateUpdateProject,
+		ProjectItem,
+		ProjectFilter,
+	},
+})
+export default class ProjectList extends Vue {
+	private projects: Project[] = [];
+	private loading: boolean = true;
+	private error: boolean = false;
+	private filters: any = {
+		field: '',
+		name: '',
+		collaborator: this.$utils.getUserId(),
+		limit: PAGE_SIZE,
+		offset: 0,
+	};
+	private projectsCount: number = 0;
+
+	private page: number = 1;
+	private PAGE_SIZE: number = PAGE_SIZE;
+
+	public mounted(): void {
+		this.getProjectList();
+	}
+
+	private getFilter(page: number): any {
+		const field = this.$route.query.field || this.filters.field;
+		const query = this.$route.query.query || this.filters.query;
+		const params: any = {
+			[field]: query,
+			collaborator: this.filters.collaborator,
+			limit: this.filters.limit,
+			offset: (page - 1) * PAGE_SIZE,
+		};
+
+		return params;
+	}
+
+	private getProjectList(page: number = 1) {
+		const params = this.getFilter(page);
+
+		Vue.$axios
+			.get('/project', { params })
+			.then((response: AxiosResponse) => {
+				this.projects = response.data.results;
+				this.projectsCount = response.data.count;
+			})
+			.catch(() => (this.error = true))
+			.finally(() => (this.loading = false));
+	}
+
+	private getAllProjectsList(page: number = 1) {
+		const params = this.getFilter(page);
+
+		Vue.$axios
+			.get('/project/projectlist', { params })
+			.then((response: AxiosResponse) => {
+				this.projects = response.data.results;
+				this.projectsCount = response.data.count;
+			})
+			.catch(() => (this.error = true))
+			.finally(() => (this.loading = false));
+	}
+
+	private getProjectDetails(project: Project) {
+		if (project.id) {
+			this.$router.push({
+				name: 'project-details',
+				params: { id: project.id.toString() },
+			});
+		}
+	}
+
+	private onApplyFilter(query: string, field: string, showUserProjects: string) {
+		this.filters.query = query;
+		console.log("check apply filter")
+		this.filters.field = field;
+		if (showUserProjects) {
+			this.filters.collaborator = this.$utils.getUserId();
+		} else {
+			this.filters.collaborator = '';
+		}
+		this.getProjectList();
+	}
+}
+</script>
+
+<style scoped>
+.project-list {
+	padding: 0;
+	font-size: .875rem;
+}
+.card-projects {
+	padding: 20px 0;
+}
+</style>
